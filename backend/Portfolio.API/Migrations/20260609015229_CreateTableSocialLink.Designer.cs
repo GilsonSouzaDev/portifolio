5e9bc7f26b14eda4731c99471cab9ed4e86c0636
GilsonSouzaDev <gilsonsouza.dev@gmail.com>
2026-06-08 22:53:41 -0300

    feat: cria tabela social_link closes #51

--- backend/Portfolio.API/Migrations/20260609015229_CreateTableSocialLink.Designer.cs
+++ backend/Portfolio.API/Migrations/20260609015229_CreateTableSocialLink.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Oracle.EntityFrameworkCore.Metadata;
 using Portfolio.API.Data;
@@ -11,9 +12,11 @@ using Portfolio.API.Data;
 namespace Portfolio.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260609015229_CreateTableSocialLink")]
+    partial class CreateTableSocialLink
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/Portfolio.API/Migrations/20260609015229_CreateTableSocialLink.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portfolio.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableSocialLink : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SocialLinks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Platform = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Url = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    IconUrl = table.Column<string>(type: "NVARCHAR2(2000)", nullable: true),
+                    DisplayOrder = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SocialLinks", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SocialLinks");
+        }
+    }
+}
