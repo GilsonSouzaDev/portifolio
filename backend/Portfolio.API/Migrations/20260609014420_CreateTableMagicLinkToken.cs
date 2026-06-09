4991e5154e5239dd7d69db21c94d1e5531c2191c
GilsonSouzaDev <gilsonsouza.dev@gmail.com>
2026-06-08 22:45:38 -0300

    feat: cria tabela magic_link_token closes #50

---
+++ backend/Portfolio.API/Migrations/20260609014420_CreateTableMagicLinkToken.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portfolio.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableMagicLinkToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MagicLinkTokens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Token = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    IsUsed = table.Column<bool>(type: "NUMBER(1)", nullable: false),
+                    ExpiresAt = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MagicLinkTokens", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MagicLinkTokens");
+        }
+    }
+}
