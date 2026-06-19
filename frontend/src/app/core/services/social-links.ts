be02caf7daae52f50157a24b255b323afd96d638
GilsonSouzaDev <gilsonsouza.dev@gmail.com>
2026-06-19 00:25:14 -0300

    feat: cria services de profile, skills, projects e social-links

---
+++ frontend/src/app/core/services/social-links.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface SocialLink {
+  id: number;
+  platform: string;
+  url: string;
+  iconUrl: string | null;
+  displayOrder: number;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SocialLinksService {
+  private apiUrl = 'http://localhost:5217/api/social-links';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<SocialLink[]> {
+    return this.http.get<SocialLink[]>(this.apiUrl);
+  }
+
+  update(id: number, link: SocialLink): Observable<SocialLink> {
+    return this.http.put<SocialLink>(`${this.apiUrl}/${id}`, link);
+  }
+}
